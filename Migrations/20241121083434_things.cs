e2b40b107f3c49cf9d3fa44df9059e505d3d7faf
ymankh <ymankh1997@gmail.com>
2024-11-21 11:35:51 +0300

    update the database

---
+++ Migrations/20241121083434_things.cs
@@ -0,0 +1,48 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace SchoolSystemTask.Migrations
+{
+    /// <inheritdoc />
+    public partial class things : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Facebook",
+                table: "Teachers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Instagram",
+                table: "Teachers",
+                type: "TEXT",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "Twitter",
+                table: "Teachers",
+                type: "TEXT",
+                nullable: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Facebook",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "Instagram",
+                table: "Teachers");
+
+            migrationBuilder.DropColumn(
+                name: "Twitter",
+                table: "Teachers");
+        }
+    }
+}
